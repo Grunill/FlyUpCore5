603e1c33c555cf3f9bf6938d84bb71ff9385c945
Jason Grunill <jason_g@coop.co.uk>
2021-05-07 11:22:35 +0100

    Add Models: Video, Activity

---
+++ src/FlyUpCore5/FlyUpCore5/Models/Activity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlyUpCore5.Models
+{
+    public class Activity
+    {
+        public Activity()
+        {
+            Videos = new List<Video>();
+        }
+        public int Id { get; set; }
+        [Required, StringLength(200)]
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Video> Videos { get; set; }
+    }
+}
---
+++ src/FlyUpCore5/FlyUpCore5/Models/Video.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlyUpCore5.Models
+{
+    public class Video
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Activity")]
+        public int ActivityId { get; set; }
+
+        [Required, StringLength(20)]
+        public string Title { get; set; }
+
+        [Required, StringLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public int ViewCount { get; set; }
+
+        public Activity Activity { get; set; }
+
+        public string CoverImageFileName
+        {
+            get
+            {
+                return Title.Replace(" ", "-") + ".jpg";
+            }
+        }
+    }
+}
