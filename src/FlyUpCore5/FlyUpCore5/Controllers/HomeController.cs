677f2453b7a9326bf4a4860f905693102129e8fd
Jason Grunill <jason_g@coop.co.uk>
2021-06-11 12:17:48 +0100

    Add About View

--- src/FlyUpCore5/FlyUpCore5/Controllers/HomeController.cs
+++ src/FlyUpCore5/FlyUpCore5/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace FlyUpCore5.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
